7a4cb64e210ea9e7e4d5a1f518792994ade29569
michael-nhat <nhathoang.haiphong@gmail.com>
2022-12-15 21:05:43 +0700

    delete repeated script

---
+++ delete-1-script.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import sys;
+import re;
+
+# input file must have 2  baidu-script tag
+fileInName = sys.argv[1]
+with open(fileInName, 'r+') as fileIn:
+    strIn = fileIn.read()
+    regexStart = r"<script>\s*var _hmt ="
+    regexEnd = r"insertBefore\(hm, s\);\s*\}\)\(\);\s*</script>"
+
+    r1 = re.findall(regexStart, strIn)
+    r2 = re.findall(regexEnd, strIn)
+
+    firstPosStr = r1[0]
+    secondPosStr = r2[0]
+
+    firstPos = strIn.find(firstPosStr);
+    secondPos = strIn.find(secondPosStr) + len(secondPosStr);
+    resultStr = strIn[:firstPos - 1] + strIn[secondPos+1:]
+    # print resultStr
+    fileIn.seek(0)
+    fileIn.write(resultStr)
+    fileIn.truncate()
+
+    fileIn.close()
--- run-find-no-code.sh
+++ run-find-no-code.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 find-no-code () {
     if grep -q 'a87ff42a01e584a69e3421eecad7cb31' "$1"; then
-    echo "has right script: $1";
-    true;
+    # check if 2 script, delete one, then replace one
+        if [[ $(grep $1 -e "hm.src =" | wc -l) -gt 1 ]];
+        then 
+            echo "found 2 script: $1 need delete one and replace one";
+            ./delete-1-script.py $1; 
+            ./awk-replace.awk $1  | sponge $1;
+        else
+        echo "has right script: $1";
+        fi
     else if grep -q 'hm.src =' "$1"; then
-    echo "has script but no right code: $1";
-    # replace here
-    ./awk-replace.awk $1  | sponge $1;
-    else
-    echo "need insert script: $1";
-    ./insert-script.py $1;
-    fi
+        echo "has script but no right code: $1";
+        # replace here
+        ./awk-replace.awk $1  | sponge $1;
+        else
+        echo "need insert script: $1";
+        ./insert-script.py $1;
+        fi
     fi
 }
 
